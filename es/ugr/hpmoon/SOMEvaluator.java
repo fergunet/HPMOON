a1c0b32c1bbda06e69cdb5b0ed29786ff427b039
Pablo García Sánchez <fergunet@gmail.com>
2015-06-29 18:50:49 +0200

    First test

---
+++ es/ugr/hpmoon/SOMEvaluator.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.ugr.hpmoon;
+
+import ec.EvolutionState;
+import ec.Individual;
+import ec.Problem;
+import ec.multiobjective.MultiObjectiveFitness;
+import ec.simple.SimpleProblemForm;
+import ec.util.Parameter;
+import ec.vector.DoubleVectorIndividual;
+
+/**
+ *
+ * @author pgarcia
+ */
+public class SOMEvaluator extends Problem implements SimpleProblemForm{
+
+    @Override
+    public void setup(final EvolutionState state, final Parameter base){
+        super.setup(state, base);
+    }
+    
+    @Override
+    public void evaluate(EvolutionState state, Individual ind, int subpopulation, int threadnum) {
+        if( !( ind instanceof DoubleVectorIndividual ) )
+            state.output.fatal( "The individuals for this problem should be DoubleVectorIndividuals." );
+
+        DoubleVectorIndividual temp = (DoubleVectorIndividual)ind;
+        double[] genome = temp.genome;
+        int numDecisionVars = genome.length;
+
+        double[] objectives = ((MultiObjectiveFitness)ind.fitness).getObjectives();
+        
+        
+        
+        //////////////////////
+        double f, g, h, sum;
+        f = genome[0];
+        objectives[0] = f;
+        sum = 0;
+        for(int i = 1; i< numDecisionVars; ++i)
+                    sum += genome[i];
+        g = 1d+9d*sum/(numDecisionVars - 1.0);
+        h = 1d-Math.sqrt(f/g);
+        objectives[1] = (g*h);
+        //////////////////////
+        
+        
+        ((MultiObjectiveFitness)ind.fitness).setObjectives(state, objectives);
+        ind.evaluated = true;
+    }
+    
+}
